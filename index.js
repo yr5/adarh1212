3fa6a0c8d8e6b9551ef55e14555d3d94fa0b47b4
yr5 <39791273+yr5@users.noreply.github.com>
2018-08-13 10:04:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -311,9 +311,9 @@ client.on('message',function(message) {
        if(!muteRole) return message.guild.createRole({name: 'Muted'}).then(message.guild.channels.forEach(chan => chan.overwritePermissions(muteRole, {SEND_MESSAGES:false,ADD_REACTIONS:false})));
        if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة``');
        if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة مني``');
-       
-       if(!muteMember) return message.channel.send('ℹ **Error:** ``منشن شخص``');
        let muteMember = message.mentions.members.first();
+       if(!muteMember) return message.channel.send('ℹ **Error:** ``منشن شخص``');
+       
        let muteReason = messageArray[2];
        if(!muteReason) return message.channel.send('ℹ **Error:** ``حدد سباّ``');
        let muteDuration = messageArray[3];
